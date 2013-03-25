9933d60b6e0ecea2529b40d2a992c80e9ca6db82
Hugo Roque <hugolnx@gmail.com>
2013-03-25 08:48:06 -0300

    updating use.sh

--- use.sh
+++ use.sh
@@ -2,5 +2,9 @@ mkdir ~/.vim/
 mkdir ~/.vim/backup
 mkdir ~/.vim/tmp
 mkdir ~/.vim/skeletons
+mkdir ~/.vim/autoload
+mkdir ~/.vim/bundle
 cp -rf ./vim/skeletons/* ~/.vim/skeletons
+cp -rf ./vim/autoload/* ~/.vim/autoload
+cp -rf ./vim/bundle/* ~/.vim/bundle
 cp ./vimrc ~/.vimrc
