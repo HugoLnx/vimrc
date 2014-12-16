8b1a3069625a3ef1e661b3e1cdd23b7da67a4222
Hugo Roque <hugolnx@gmail.com>
2014-12-16 21:40:30 -0200

    creating syntax/ on use.sh

--- use.sh
+++ use.sh
@@ -3,5 +3,7 @@ mkdir ~/.vim/backup
 mkdir ~/.vim/tmp
 mkdir ~/.vim/skeletons
 mkdir ~/.vim/bundle
+mkdir ~/.vim/syntax
 cp -rf ./vim/skeletons/* ~/.vim/skeletons
+cp -rf ./vim/syntax/* ~/.vim/syntax
 cp ./vimrc ~/.vimrc
