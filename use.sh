ac345ea59c8c5d02203e4710a1be1985660d1e62
Hugo Roque <hugo.figueiredo@corp.globo.com>
2015-01-07 18:20:30 -0200

    use.sh: using option -p in mkdir to avoid conflict errors

--- use.sh
+++ use.sh
@@ -1,9 +1,9 @@
-mkdir ~/.vim/
-mkdir ~/.vim/backup
-mkdir ~/.vim/tmp
-mkdir ~/.vim/skeletons
-mkdir ~/.vim/bundle
-mkdir ~/.vim/syntax
+mkdir -p ~/.vim/
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/tmp
+mkdir -p ~/.vim/skeletons
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/syntax
 cp -rf ./vim/skeletons/* ~/.vim/skeletons
 cp -rf ./vim/syntax/* ~/.vim/syntax
 cp ./vimrc ~/.vimrc
