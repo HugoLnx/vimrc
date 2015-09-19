1620a519563fc94a7e05ed313898f80927290d40
HugoLnx <hugolnx@gmail.com>
2015-09-19 07:27:40 -0300

    use.sh: Installing the plugins

--- use.sh
+++ use.sh
@@ -8,3 +8,7 @@ cp -rf ./vim/skeletons/* ~/.vim/skeletons
 cp -rf ./vim/syntax/* ~/.vim/syntax
 cp ./vimrc ~/.vimrc
 cp ./gitconfig ~/.gitconfig
+
+# Installing the plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
