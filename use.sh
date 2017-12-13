bfa966270b75d9b945fae9c74757cc0a01725da7
Hugo Roque <hugolnx@gmail.com>
2017-12-13 11:40:42 -0200

    Backing up files before running

--- use.sh
+++ use.sh
@@ -1,3 +1,8 @@
+timestamp=$(date +%Y%m%d%H%M%S)
+if [ -d ~/.vim ];then
+	cp -rf ~/.vim ~/.vim_${timestamp}
+fi
+
 mkdir -p ~/.vim/
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/tmp
@@ -7,6 +12,10 @@ mkdir -p ~/.vim/syntax
 cp -rf ./vim/skeletons/* ~/.vim/skeletons
 cp -rf ./vim/syntax/* ~/.vim/syntax
 cp ./vimrc ~/.vimrc
+
+if [ -f ~/.gitconfig ];then
+    cp ~/.gitconfig ~/.gitconfig.bkp_${timestamp}
+fi
 cp ./gitconfig ~/.gitconfig
 
 # Installing the plugins
